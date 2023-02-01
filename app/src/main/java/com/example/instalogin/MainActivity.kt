b6f10e8b7a6f0b5798c6814d6f05b31f2ac368ad
ansh1119 <anshsingh1119@gmail.com>
2023-02-01 18:00:37 +0530

    Front page of insta login page

--- app/src/main/java/com/example/instalogin/MainActivity.kt
+++ app/src/main/java/com/example/instalogin/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.instalogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val x:Button=findViewById(R.id.Login)
+        x.setOnClickListener(){
+            Toast.makeText(this,"LOGGING IN",Toast.LENGTH_SHORT).show()
+        }
+
+        }
     }
-}
