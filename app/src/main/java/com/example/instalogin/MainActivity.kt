8bb219b175a1bec73344a82d0daa166158dde075
ansh1119 <anshsingh1119@gmail.com>
2023-02-02 18:19:13 +0530

    added a new activity after clicking on the login button

--- app/src/main/java/com/example/instalogin/MainActivity.kt
+++ app/src/main/java/com/example/instalogin/MainActivity.kt
@@ -3,18 +3,19 @@ package com.example.instalogin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val x:Button=findViewById(R.id.Login)
-        x.setOnClickListener(){
-            Toast.makeText(this,"LOGGING IN",Toast.LENGTH_SHORT).show()
-        }
-
+        val x:AppCompatButton=findViewById(R.id.Login)
+        x.setOnClickListener{
+            Toast.makeText(this,"Loggin in",Toast.LENGTH_SHORT).show()
+            intent=Intent(this,afterlogin::class.java)
+            startActivity(intent)
         }
     }
+}
---
+++ app/src/main/java/com/example/instalogin/afterlogin.kt
@@ -0,0 +1,11 @@
+package com.example.instalogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class afterlogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_afterlogin)
+    }
+}
